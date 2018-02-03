224caffc2791660763fdd742d365ea7d20c3fe43
Phonethip <phonethip.liu@gmail.com>
2018-02-03 01:29:19 -0600

    have created objects and outout in DOM but have to work on DOM styling of card headers

--- main.js
+++ main.js
@@ -9,31 +9,63 @@
 // Output the HTML strings with classes to the DOM that you can style with CSS.
 // Refactor "makeFrontMessage" to return something more elaborate.
 
+//====== Let see it in the DOM ======//
+var domText = document.getElementById("greeting-card");
 
-let domText = document.getElementById("greeting-card");
-
+//====== constructor for CardMaker Object ======//
 const CardMaker = {
     frontMessage: function(string) {
-      return `<h2>${this.frontMessage}</h2>` 
+      return `<h1>${frontMessage}</h1>` 
+      // return `<h1>${this.frontMessage}</h1>` 
       // console.log(this.FrontMessage)
     },
 
     insideMessage: function(string) {
-        return `<h2>${this.insideMessage}</h2>`
+      return `<h3>${insideMessage}</h3>`
+      // return `<h3>${this.insideMessage}</h3>`
         // console.log(this.BackMessage)
       }
-  };
+  }
+//Testing the object functions
+  // console.log(CardMaker);
+  // console.log(CardMaker.frontMessage());
+  // console.log(CardMaker.insideMessage());
 
-CardMaker.prototype.greeting = function() {
-    return "<div>" + this.frontMessage + this.insideMessage + "</div>"
-  };
+// CardMaker.prototype.Greeting = function(){
+//   return this.frontMessage + '<div>' + this.insideMessage + '</div>';
+//}
 
-const BirthdayCard = Object.create(CardMaker);
-BirthdayCard.frontMessage = "Happy Birthday!"
-BirthdayCard.backMessage = "Eat, drink and drink some more.....BUT please stop drinking when you think walking around in your birthday suit in public would be good idea to celebrate the day of your birth."
+// ===== Birthday Card ======== //
 
-domText.innerHTML = BirthdayCard.frontMessage();
-domText.innerHTML= BirthdayCard.backMessage();
+const Birthday = Object.create(CardMaker);
+
+Birthday.frontMessage = "Happy Birthday!";
+Birthday.insideMessage = "Eat, drink and drink some more.....BUT please stop drinking when you think walking around in your birthday suit in public would be good idea to celebrate the day of your birth.";
+Birthday.style = document.getElementById("greeting-card").style.color = "red";
+//console.log(Birthday.frontMessage, Birthday.insideMessage);
+domText.innerHTML += Birthday.frontMessage + "<div>" + Birthday.insideMessage + "</div><br>";
+
+
+// ===== Anniversary Card ======== //
+
+const Anniversary = Object.create(CardMaker);
+
+Anniversary.frontMessage = "I remembered this year...";
+Anniversary.insideMessage = "Happy Anniversary baby! Thank you for not leaving me when I forgot last year.";
+//console.log(Anniversary.frontMessage, Anniversary.insideMessage);
+domText.innerHTML += Anniversary.frontMessage + "<div>" + Anniversary.insideMessage + "</div><br>";
+
+
+// ===== Valentine Card ======== //
+
+const Valentine = Object.create(CardMaker);
+
+Valentine.frontMessage = "Be mine Valentine";
+Valentine.insideMessage = "Life is sweeter having you in my life. Happy Valentine's Day my Love.";
+//console.log(Valentine.frontMessage, Valentine.insideMessage);
+domText.innerHTML += Valentine.frontMessage + "<div>" + Valentine.insideMessage + "</div><br>";
+
+
 
 // ==== Refactor this to link this object to cardMaker =====
 // const BirthdayCard = Object.create(CardMaker);
