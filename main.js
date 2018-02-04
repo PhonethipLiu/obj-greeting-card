db309d1ab2e40cd9f99709ef7140bcac80f82905
Phonethip <phonethip.liu@gmail.com>
2018-02-04 03:08:58 -0600

    DOM elements change

--- main.js
+++ main.js
@@ -10,18 +10,17 @@
 // Refactor "makeFrontMessage" to return something more elaborate.
 
 //====== Let see it in the DOM ======//
-var domText = document.getElementById("greeting-card");
+var cardDiv = document.getElementById("card");
 
 //====== constructor for CardMaker Object ======//
 const CardMaker = {
     frontMessage: function(string) {
-      return `<h1>${frontMessage}</h1>` 
+      return `<h2>${frontMessage}</h2>` 
       // return `<h1>${this.frontMessage}</h1>` 
       // console.log(this.FrontMessage)
     },
-
     insideMessage: function(string) {
-      return `<h3>${insideMessage}</h3>`
+      return `<p>${insideMessage}</p>`
       // return `<h3>${this.insideMessage}</h3>`
         // console.log(this.BackMessage)
       }
@@ -31,39 +30,33 @@ const CardMaker = {
   // console.log(CardMaker.frontMessage());
   // console.log(CardMaker.insideMessage());
 
-// CardMaker.prototype.Greeting = function(){
-//   return this.frontMessage + '<div>' + this.insideMessage + '</div>';
-//}
-
 // ===== Birthday Card ======== //
 
 const Birthday = Object.create(CardMaker);
-
 Birthday.frontMessage = "Happy Birthday!";
 Birthday.insideMessage = "Eat, drink and drink some more.....BUT please stop drinking when you think walking around in your birthday suit in public would be good idea to celebrate the day of your birth.";
-Birthday.style = document.getElementById("greeting-card").style.color = "red";
 //console.log(Birthday.frontMessage, Birthday.insideMessage);
-domText.innerHTML += Birthday.frontMessage + "<div>" + Birthday.insideMessage + "</div><br>";
+cardDiv.innerHTML += "<div><h2>" + Birthday.frontMessage  + "</h2>" + "<p>" + Birthday.insideMessage + "</p></div>";
+// Birthday.style = cardDiv[1].bgColor = "black";
+
 
 
 // ===== Anniversary Card ======== //
 
 const Anniversary = Object.create(CardMaker);
-
 Anniversary.frontMessage = "I remembered this year...";
 Anniversary.insideMessage = "Happy Anniversary baby! Thank you for not leaving me when I forgot last year.";
 //console.log(Anniversary.frontMessage, Anniversary.insideMessage);
-domText.innerHTML += Anniversary.frontMessage + "<div>" + Anniversary.insideMessage + "</div><br>";
+cardDiv.innerHTML += "<div><h2>"+ Anniversary.frontMessage + "</h2>" + "<p>" + Anniversary.insideMessage + "</p></div>";
 
 
 // ===== Valentine Card ======== //
 
 const Valentine = Object.create(CardMaker);
-
 Valentine.frontMessage = "Be mine Valentine";
 Valentine.insideMessage = "Life is sweeter having you in my life. Happy Valentine's Day my Love.";
 //console.log(Valentine.frontMessage, Valentine.insideMessage);
-domText.innerHTML += Valentine.frontMessage + "<div>" + Valentine.insideMessage + "</div><br>";
+cardDiv.innerHTML += "<div><h2>" + Valentine.frontMessage + "</h2>" + "<p>" + Valentine.insideMessage + "</p></div>";
 
 
 
