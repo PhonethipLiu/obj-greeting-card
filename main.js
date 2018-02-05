905031e2ca1aad18492bedab3e0f0761cf694bb3
Phonethip <phonethip.liu@gmail.com>
2018-02-05 01:31:10 -0600

    still working on output DOM classes

--- main.js
+++ main.js
@@ -9,8 +9,13 @@
 // Output the HTML strings with classes to the DOM that you can style with CSS.
 // Refactor "makeFrontMessage" to return something more elaborate.
 
-//====== Let see it in the DOM ======//
+//====== Let's see it in the DOM ======//
 var cardDiv = document.getElementById("card");
+// var addClass = cardDiv.className += "bday-card";
+// cardDiv.style.backgroundColor = "lightgray";
+// var paragraph = document.createAttribute("");
+// cardDiv.className = "colorBox";
+// var div = document.getElementsByTagName("DIV")[0].className;
 
 //====== constructor for CardMaker Object ======//
 const CardMaker = {
@@ -37,7 +42,8 @@ Birthday.frontMessage = "Happy Birthday!";
 Birthday.insideMessage = "Eat, drink and drink some more.....BUT please stop drinking when you think walking around in your birthday suit in public would be good idea to celebrate the day of your birth.";
 //console.log(Birthday.frontMessage, Birthday.insideMessage);
 cardDiv.innerHTML += "<div><h2>" + Birthday.frontMessage  + "</h2>" + "<p>" + Birthday.insideMessage + "</p></div>";
-// Birthday.style = cardDiv[1].bgColor = "black";
+var background = cardDiv.style.backgroundColor;
+Birthday.background  = "lightgray";
 
 
 
